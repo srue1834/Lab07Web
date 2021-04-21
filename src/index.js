cd45d117ffffcf5677f9579f3fec4fe5d6141b70
srue1834 <53914141+srue1834@users.noreply.github.com>
2021-04-21 17:15:04 -0600

    mi boilerplate

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div>
+    <h1>Hola</h1>
+
+  </div>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+)
