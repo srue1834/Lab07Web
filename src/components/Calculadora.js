a6da96d0a732172f2b7db4f8f555f30b010b4912
srue1834 <53914141+srue1834@users.noreply.github.com>
2021-05-05 17:37:13 -0600

    Cambios

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -1,22 +1,37 @@
+/* eslint-disable camelcase */
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react'
 import PropTypes from 'prop-types'
 import './Calculadora.css'
 
-const Calculadora = ({ value, onClick }) => (
+const calc_buttons = ({ value, onClick }) => (
   <div className="fondo">
-    {value}
     <div className="pantalla">{value}</div>
-    <button className="boton" type="button" onClick={onClick}>C</button>
-    <button className="boton" type="button" onClick={onClick}>7</button>
+    <div className="container">
+      <button className="boton" type="button" onClick={onClick}>7</button>
+      <button className="boton" type="button" onClick={onClick}>8</button>
+      <button className="boton" type="button" onClick={onClick}>9</button>
+      <button className="boton" type="button" onClick={onClick}>÷</button>
+      <button className="boton" type="button" onClick={onClick}>4</button>
+      <button className="boton" type="button" onClick={onClick}>5</button>
+      <button className="boton" type="button" onClick={onClick}>6</button>
+      <button className="boton" type="button" onClick={onClick}>x</button>
+      <button className="boton" type="button" onClick={onClick}>1</button>
+      <button className="boton" type="button" onClick={onClick}>2</button>
+      <button className="boton" type="button" onClick={onClick}>3</button>
+      <button className="boton" type="button" onClick={onClick}>+</button>
+      <button className="boton" type="button" onClick={onClick}>+/-</button>
+      <button className="boton" type="button" onClick={onClick}>0</button>
+      <button className="boton" type="button" onClick={onClick}>.</button>
+      <button className="boton" type="button" onClick={onClick}>=</button>
+    </div>
   </div>
-
 )
 
-Calculadora.propTypes = {
+calc_buttons.propTypes = {
   value: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
 }
 
-export default Calculadora
+export default calc_buttons
